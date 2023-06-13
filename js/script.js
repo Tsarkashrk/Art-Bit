9d175cfc46b46aa5f419e8ff88ee08b97a0b5224
Alisher Tokesh <tokesh04@mail.ru>
2023-06-13 15:56:14 +0600

    #12: Added reviews section. #13: Added developers section

--- js/script.js
+++ js/script.js
@@ -6,3 +6,35 @@ new Swiper(
     freeMode: true,
   }
 );
+
+new Swiper(
+  '.element-slider', 
+  {
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  }
+);
+
+new Swiper(
+  '.reviews-slider', 
+  {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      834: {
+        slidesPerView: 2.5,
+        spaceBetween: 10,
+      },
+      1200: {
+        slidesPerView: 3,
+      }
+    }
+  }
+)
