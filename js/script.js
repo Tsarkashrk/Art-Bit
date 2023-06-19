7e4eeb949f85d6879ab145728241178269aad965
Alisher Tokesh <tokesh04@mail.ru>
2023-06-19 14:17:54 +0600

    revert last features

--- js/script.js
+++ js/script.js
@@ -1,40 +1,8 @@
 new Swiper(
-  '.platforms__slider', 
+  '.image-slider', 
   {
     slidesPerView: 3.5,
     spaceBetween: 12,
     freeMode: true,
   }
 );
-
-new Swiper(
-  '.subscription__slider', 
-  {
-    slidesPerView: 1,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  }
-);
-
-new Swiper(
-  '.reviews__slider', 
-  {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      834: {
-        slidesPerView: 2.5,
-        spaceBetween: 10,
-      },
-      1200: {
-        slidesPerView: 3,
-      }
-    }
-  }
-)
